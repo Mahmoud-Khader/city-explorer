e6c5e9e1bfcbaabca04180179665692f5660b986
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-07-04 16:05:43 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      mapSearch: '',
+      show: false,
+      cityData: {}
+
+    }
+  }
+  explore = async (e) => {
+    e.preventDefault();
+
+    
+    await this.setState({
+      mapSearch: e.target.city.value
+    })
+console.log(e.target.city.value)
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_lOCATIONIQ_KEY}&q=${this.state.mapSearch}&format=json`
+
+    let resData = await axios.get(url);
+
+    // console.log(resData)
+    // console.log(resData.data)
+    // console.log(resData.data[0])
+
+    this.setState({
+      show: true,
+      cityData: resData.data[0]
+
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.explore}>
+          <input type='text' placeholder='Enter The City Name' name='city' />
+          <input type='submit' value='Explore'  />
+
+        </form>
+
+        <p>City Name : {this.state.cityData.display_name} , Latitude : {this.state.cityData.lat} , longitude : {this.state.cityData.lon}</p>
+
+        {this.state.show && 
+        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_lOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=13`} alt=''/>
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
